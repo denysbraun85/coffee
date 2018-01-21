2c85345b823389389d227616046663a89acff135
Denys <denysbraun1985@gmail.com>
2018-01-22 00:22:56 +0200

    finish 1024px company page and add link on main page

--- about-company/js/script.js
+++ about-company/js/script.js
@@ -1,4 +1,4 @@
-//-----------------------------------===============================
+//-----------------------------------===============================Full
 //count for message
  $(function() {
      $("input[id='message']").keyup(function countRemainingChars(){
@@ -13,7 +13,7 @@
      });
  });
 
-//-----------------------------------===============================
+//-----------------------------------===============================Full
 //random code for verification
  function randomInteger(min, max) {
      var write = document.getElementById('code-number');
@@ -25,7 +25,7 @@
 
 //randomInteger (100000, 999999);
 
-//-----------------------------------===============================
+//-----------------------------------===============================Full
  //function validateEmail(){
  //    //var emailReg = /^([\w]+@([\w-]+\.)+[\w-]{2,4})?$/;
  //    var inputEmail = document.getElementById('email').value;
@@ -39,31 +39,41 @@
  //    }
  //}
 
-//-----------------------------------===============================
+//-----------------------------------===============================Full
 // $('.modal-button').on('click', function (){          //something wrong with this code
 //         $('.modal-text-2').css("opacity", "1");
 //         console.log('safdfsf');
 // });
 
-//-----------------------------------===============================
+//-----------------------------------===============================Full
 //Modal window for REST OF PAGES
  function ModalAll() {
      // Get modal element
      var modalAll = document.getElementById('registration-modal');
+
      // Get open modal button
      var modalBtnAll = document.getElementById('log-img');
      var modalImgAll = document.getElementsByClassName('log-img')[0];
-     // Get close button
-     var closeBtnAll = document.getElementById('close');
+
      // Get filter
      var filter = document.getElementsByClassName('wrapper')[0];
+
+     //Get open modal on 1024px
+     var modal1024px = document.getElementsByClassName('user-log-icon')[0];
+     var menuBar = document.getElementsByClassName('menu-bar')[0];
+
+     // Get close button
+     var closeBtnAll = document.getElementById('close');
+     var lines768px = document.getElementsByClassName('three-lines')[0];
 
      // Listen for open click
      modalBtnAll.addEventListener('click', openModalAll);
      modalImgAll.addEventListener('click', openModalAll);
+     modal1024px.addEventListener('click', openModalAll);
 
      // Listen for close click
      closeBtnAll.addEventListener('click', closeModalAll);
+     lines768px.addEventListener('click',closeModalAll);
      // Listen for outside click
      window.addEventListener('click', outsideClickAll);
 
@@ -71,7 +81,8 @@
      function openModalAll(){
          modalAll.style.display = 'block';
          //Get dark background filter for page
-         filter.style.backgroundColor = 'rgba(0,0,0,.3)';
+         $('.menu-bar').addClass('back-grey');
+         // filter.style.backgroundColor = 'rgba(0,0,0,.3)'; This is working so do not delete. May it be here for the first time
          //Get scroll OFF
          var curScrollTop = $(window).scrollTop();
          $('html').toggleClass('noscroll').css('top', '-' + curScrollTop + 'px');
@@ -81,28 +92,36 @@
      // Function to close modal
      function closeModalAll(){
          modalAll.style.display = 'none';
-         filter.style.filter = 'none';
+         // filter.style.filter = 'none'; This is working so do not delete. May it be here for the first time
          //Get OFF dark background filter for page
-         filter.style.backgroundColor = 'rgba(0,0,0,0)';
+         $('.menu-bar').removeClass('back-grey');
+         // filter.style.backgroundColor = 'rgba(0,0,0,0)'; This is working so do not delete. May it be here for the first time
          //Get  scroll ON
          $('html').toggleClass('noscroll');
      }
 
      // Function to close modal if outside click
      function outsideClickAll(e){
-         if(e.target === modalAll){
+         if(e.target === modalAll || e.target === menuBar){
+             $('#registration-modal').toggleClass('close-window-modal');
              modalAll.style.display = 'none';
-             filter.style.filter = 'none';
+             // filter.style.filter = 'none'; This is working so do not delete. May it be here for the first time
              //Get OFF dark background filter for page
-             filter.style.backgroundColor = 'rgba(0,0,0,0)';
+             // filter.style.backgroundColor = 'rgba(0,0,0,0)'; This is working so do not delete. May it be here for the first time
              //Get  scroll ON
+             $('#registration-modal').css('display','none');
+             $('.menu-bar').removeClass('back-grey');
              $('html').toggleClass('noscroll');
          }
      }
+     //Function to get no-scroll on three-lines click
+     $('.three-lines').on('click', function () {
+         $('html').toggleClass('noscroll');
+     })
  }
  ModalAll();
 
- //-----------------------------------===============================
+ //-----------------------------------===============================Full
  //Scroll on all page
  $(function() {
 
@@ -110,14 +129,14 @@
 
  });
 
- //-----------------------------------===============================
+ //-----------------------------------===============================Full
  //Transition function for preloader on modal window
  $('#modal-button').on('click',function () {
      $('#linePreloader').toggleClass("lineClick");
      console.log('bvhdfabvdffd');
  });
 
- //-----------------------------------===============================
+ //-----------------------------------===============================Full
  //Function for validation form on modal menu
  var email = $('#email').val();
  var password = $('#passw').val();
@@ -137,7 +156,7 @@
      setInterval(myInterval,5000);
  });
 
- //-----------------------------------===============================
+ //-----------------------------------===============================Full
  //Function to open and close input password on modal window in case when user forgot password
  $('.modal-text-3').on('click',function () {
      $(this).text(function(i, v){
@@ -151,7 +170,39 @@
      $('#email').toggleClass('widthEmail');
  });
 
+//-----------------------------------===============================1024
+//Scroll show and hide footer back on top button
+// $(window).scroll(function(){
+//     var top = $(window).scrollTop();
+//     if( top > 1450) {
+//         $(".container-back-to-top-1024px").css('visibility', 'visible');
+//     } else {
+//         $(".container-back-to-top-1024px").css('visibility', 'hidden');
+//     }
+// });
+//-----------------------------------===============================1024
+//Function to get on top when user will make a click on arrow up icon or under this icon text
+$('.back-icon').on('click', function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+});
 
+$('.back-text').on('click', function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+});
+
+//-----------------------------------===============================1024
+//Function to open left modal window and grey background on main page(working version with animation cross)
+$(document).ready(function(){
+    $('.three-lines').click(function(){
+        $('.side-bar-container').toggleClass('ground-grey');
+        $('.menu-bar').toggleClass('background-grey-color');
+        $(this).toggleClass('move-right');
+        $('html').toggleClass('noscroll');
+        $(this).toggleClass('change');
+    });
+});
 
 
 
