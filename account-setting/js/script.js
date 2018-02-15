7974cdc382575035ec7d917652a39fb76cd2a4eb
Denys <denysbraun1985@gmail.com>
2018-02-15 23:59:06 +0200

    fix news page with pagination

--- account-setting/js/script.js
+++ account-setting/js/script.js
@@ -1,4 +1,4 @@
-//-----------------------------------===============================
+//-----------------------------------===============================Full
 //Modal window for REST OF PAGES
  function ModalAll() {
      // Get modal element
@@ -8,6 +8,12 @@
      var modalImgAll = document.getElementsByClassName('log-img')[0];
      // Get close button
      var closeBtnAll = document.getElementById('close');
+     var lines768px = document.getElementsByClassName('three-lines')[0];
+
+     //Get open modal on 1024px
+     var modal1024px = document.getElementsByClassName('user-log-icon')[0];
+     var menuBar = document.getElementsByClassName('menu-bar')[0];
+
      // Get filter
      var filter = document.getElementById('wrap');
      var mainen = document.getElementsByTagName('main')[0];
@@ -15,9 +21,11 @@
      // Listen for open click
      modalBtnAll.addEventListener('click', openModalAll);
      modalImgAll.addEventListener('click', openModalAll);
+     modal1024px.addEventListener('click', openModalAll);
 
      // Listen for close click
      closeBtnAll.addEventListener('click', closeModalAll);
+     lines768px.addEventListener('click',closeModalAll);
      // Listen for outside click
      window.addEventListener('click', outsideClickAll);
 
@@ -25,6 +33,8 @@
      function openModalAll(){
          modalAll.style.display = 'block';
          //Get dark background filter for page
+         $('.menu-bar').addClass('back-grey');
+         $('.menu-bar').addClass('menu-bar-position');
          // filter.style.backgroundColor = 'rgba(0,0,0,.3)';
          mainen.style.backgroundColor = 'rgba(62,62,62,.5)';
          $('.main-information-container').addClass('back');
@@ -40,6 +50,8 @@
          modalAll.style.display = 'none';
          filter.style.filter = 'none';
          //Get OFF dark background filter for page
+         $('.menu-bar').removeClass('back-grey');
+         $('.menu-bar').removeClass('menu-bar-position');
          filter.style.backgroundColor = 'rgba(0,0,0,0)';
          mainen.style.backgroundColor = 'rgba(0,0,0,0)';
          $('.main-information-container').removeClass('back');
@@ -49,21 +61,33 @@
 
      // Function to close modal if outside click
      function outsideClickAll(e){
-         if(e.target === modalAll){
+         if(e.target === modalAll || e.target === menuBar){
              modalAll.style.display = 'none';
              filter.style.filter = 'none';
+             $('.menu-bar').removeClass('menu-bar-position');
+             $('#registration-modal').toggleClass('close-window-modal');
              //Get OFF dark background filter for page
              filter.style.backgroundColor = 'rgba(0,0,0,0)';
              mainen.style.backgroundColor = 'rgba(0,0,0,0)';
              $('.main-information-container').removeClass('back');
              //Get  scroll ON
+             $('#registration-modal').css('display','none');
+             $('.menu-bar').removeClass('back-grey');
              $('html').toggleClass('noscroll');
+
+             $('.menu-bar').removeClass('menu-bar-position');
+             $('.menu-bar').removeClass('background-grey-color');
          }
      }
+
+     //Function to get no-scroll on three-lines click
+     $('.three-lines').on('click', function () {
+         $('html').toggleClass('noscroll');
+     })
  }
  ModalAll();
 
-//-----------------------------------===============================
+//-----------------------------------===============================Full
 //Tabs
  $("#tab1").click(moveToFirst);
  $("#tab2").click(moveToSecond);
@@ -102,7 +126,7 @@
  }
 
 
-//-----------------------------------===============================
+//-----------------------------------===============================Full
 // //Check for tab-3
 //  if($(".item-list li input").is(":checked")) {
 //      $('.text-6').css('display','block');
@@ -116,7 +140,7 @@
 //
 //  }
 
-//-----------------------------------===============================
+//-----------------------------------===============================Full
 //Add phone number
  function add_fields() {
      var input = 1;
@@ -124,10 +148,12 @@
      var objTo = document.getElementById('phone-number-container');
      var divinput = document.createElement("div-new-phone");
      var more_fields = document.createElement("more_fields");
+     var footer = document.getElementsByTagName('footer')[0];
      divinput.innerHTML = '<div ><input type="number" id="phone"  required="required"/><label></label></div>';
 
      objTo.prepend(divinput);
      more_fields.style.marginTop = '40px';
+     footer.style.marginTop = '40px';
  }
 
  function add_fields2() {
@@ -136,10 +162,12 @@
      var objTo = document.getElementById('website-container');
      var divinput = document.createElement("div-new-web");
      var more_fields = document.createElement("more_fields-web");
+     var footer = document.getElementsByTagName('footer')[0];
      divinput.innerHTML = '<div class="group" id="web-container"><input type="email" id="web" required="required"/><label></label></div>';
 
      objTo.prepend(divinput);
      more_fields.style.marginTop = '40px';
+     footer.style.marginTop = '40px';
  }
 
  function add_fields3() {
@@ -148,10 +176,12 @@
      var objTo = document.getElementById('adress-container');
      var divinput = document.createElement("div-new-adress");
      var more_fields = document.createElement("more_adress");
+     var footer = document.getElementsByTagName('footer')[0];
      divinput.innerHTML = '<div class="group" id="adres-container"><input type="text" id="adres" required="required"/><label></label></div>';
 
      objTo.prepend(divinput);
      more_fields.style.marginTop = '40px';
+     footer.style.marginTop = '40px';
  }
 
  function add_fields4() {
@@ -160,10 +190,12 @@
      var objTo = document.getElementById('phone-number-container-2');
      var divinput = document.createElement("div-new-numb");
      var more_fields = document.createElement("more_numb");
+     var footer = document.getElementsByTagName('footer')[0];
      divinput.innerHTML = '<div class="group" id="numb"><input type="number" id="num" required="required"/><label></label></div>';
 
      objTo.prepend(divinput);
      more_fields.style.marginTop = '40px';
+     footer.style.marginTop = '40px';
  }
 
  function add_fields5() {
@@ -172,13 +204,15 @@
      var objTo = document.getElementById('website-container-2');
      var divinput = document.createElement("div-new-website");
      var more_fields = document.createElement("more_website");
+     var footer = document.getElementsByTagName('footer')[0];
      divinput.innerHTML = '<div class="group" id="site"><input type="email" id="web" required="required"/><label></label></div>';
 
      objTo.prepend(divinput);
      more_fields.style.marginTop = '40px';
+     footer.style.marginTop = '40px';
  }
 
-//-----------------------------------===============================
+//-----------------------------------===============================Full
  //Change icon to share
  $(".social-share-img1").bind("click", function() {
      var src = ($(this).attr("src") === "../img/main/sharing-interface.png")
@@ -187,7 +221,7 @@
      $(this).attr("src", src);
  });
 
-//-----------------------------------===============================
+//-----------------------------------===============================Full
  //Change icon to love
  $(".social-share-img2").bind("click", function() {
      var src = ($(this).attr("src") === "../img/main/love.png")
@@ -196,7 +230,7 @@
      $(this).attr("src", src);
  });
 
-//-----------------------------------===============================
+//-----------------------------------===============================Full
  //Check for brands, all clear
  $("#all-brands-check").change(function() {
      if(this.checked) {
@@ -224,7 +258,7 @@
 
  });
 
-//-----------------------------------===============================
+//-----------------------------------===============================Full
  //Functions for brand list. When you want change the brand all you need is click on one fo 6 brand and you will see the short list below
  $("#brand-1").change(function() {
      if(this.checked) {
@@ -352,7 +386,7 @@
      }
  });
 
-//-----------------------------------===============================
+//-----------------------------------===============================Full
 //Password function
  $('.save-pass').on('click',function () {
      var oldPassword = $("#pass").val();
@@ -388,7 +422,7 @@
       }
  });
 
- //-----------------------------------===============================
+ //-----------------------------------===============================Full
  //Scroll on all page
  $(function() {
 
@@ -396,14 +430,14 @@
 
  });
 
- //-----------------------------------===============================
+ //-----------------------------------===============================Full
  //Transition function for preloader on modal window
  $('#modal-button').on('click',function () {
      $('#linePreloader').toggleClass("lineClick");
      console.log('bvhdfabvdffd');
  });
 
- //-----------------------------------===============================
+ //-----------------------------------===============================Full
  //Function for validation form on modal menu
  var email = $('#email').val();
  var password = $('#passw').val();
@@ -423,7 +457,7 @@
      setInterval(myInterval,5000);
  });
 
- //-----------------------------------===============================
+ //-----------------------------------===============================Full
  //Function to open and close input password on modal window in case when user forgot password
  $('.modal-text-3').on('click',function () {
      $(this).text(function(i, v){
@@ -436,3 +470,39 @@
      $('#passw').toggleClass('hide-passw');
      $('#email').toggleClass('widthEmail');
  });
+
+
+//-----------------------------------===============================1024
+//Scroll show and hide footer back on top button
+// $(window).scroll(function(){
+//     var top = $(window).scrollTop();
+//     if( top > 1450) {
+//         $(".container-back-to-top-1024px").css('visibility', 'visible');
+//     } else {
+//         $(".container-back-to-top-1024px").css('visibility', 'hidden');
+//     }
+// });
+//-----------------------------------===============================1024
+//Function to get on top when user will make a click on arrow up icon or under this icon text
+$('.back-icon').on('click', function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+});
+
+$('.back-text').on('click', function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+});
+
+//-----------------------------------===============================1024
+//Function to open left modal window and grey background on main page(working version with animation cross)
+$(document).ready(function(){
+    $('.three-lines').click(function(){
+        $('.side-bar-container').toggleClass('ground-grey');
+        $('.menu-bar').toggleClass('background-grey-color');
+        $('.menu-bar').toggleClass('menu-bar-position');
+        $(this).toggleClass('move-right');
+        $('html').toggleClass('noscroll');
+        $(this).toggleClass('change');
+    });
+});
